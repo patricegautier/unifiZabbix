abe734dc8cca24d08b38fbf848c32d3caaf75e81
Patrice Gautier <patrice@mac.com>
2024-06-25 15:06:42 -0700

    better mgmt of ssh options; add -r option for RSA only devices

--- ssh-run.sh
+++ ssh-run.sh
@@ -10,6 +10,7 @@ Usage ${0}  -i privateKeyPath -p <passwordFilePath> -u user -v -d targetDevice <
 	-u SSH user name for Unifi device
 	-d IP or FQDN for Unifi device
 	-v verbose and non compressed output
+	-r explicitly allow RSA (for legacy devices, i.e Airmaxes)
 EOF
 	exit 2
 }
@@ -17,8 +18,9 @@ EOF
 declare SSHPASS_OPTIONS=
 declare PRIVKEY_OPTION=
 declare PASSWORD_FILE_PATH=
+declare SSH_OPTIONS="-o LogLevel=Error -o StrictHostKeyChecking=accept-new"
 
-while getopts 'i:u:hd:vp:' OPT
+while getopts 'i:u:hd:vp:r' OPT
 do
   case $OPT in
     i) PRIVKEY_OPTION="-i "${OPTARG} ;;
@@ -26,12 +28,13 @@ do
     d) TARGET_DEVICE=${OPTARG} ;;
     v) VERBOSE="-v" ;;
     p) PASSWORD_FILE_PATH=${OPTARG} ;;
+    r) SSH_OPTIONS+=" -o PubkeyAcceptedKeyTypes=+ssh-rsa -o HostKeyAlgorithms=+ssh-rsa" ;;
     *) usage ;;
   esac
 done
 
 shift $((OPTIND-1))
-REMOTE_COMMAND=$*
+declare -a REMOTE_COMMAND=( "$@ ")
 
 # {$UNIFI_SSHPASS_PASSWORD_PATH} means the macro didn't resolve in Zabbix
 if [[ -n "${PASSWORD_FILE_PATH}" ]] && [[ "${PASSWORD_FILE_PATH}" != "{\$UNIFI_SSHPASS_PASSWORD_PATH}" ]]; then 
@@ -41,13 +44,13 @@ fi
 
 if [[ -n "${VERBOSE:-}" ]]; then
 	# shellcheck disable=SC2086
-   	echo "${SSHPASS_COMMAND}" 'ssh -o LogLevel=Error -o StrictHostKeyChecking=accept-new ' ${PRIVKEY_OPTION} "${USER}@${TARGET_DEVICE}" "${REMOTE_COMMAND}"
+   	echo "${SSHPASS_COMMAND}" ssh ${SSH_OPTIONS} ${PRIVKEY_OPTION} "${USER}@${TARGET_DEVICE}" "${REMOTE_COMMAND[@]}"
 fi
 
 if [[ -n "${SSHPASS_OPTIONS}" ]]; then
 	# shellcheck disable=SC2086
-	sshpass "${SSHPASS_OPTIONS}" ssh -o LogLevel=Error -o StrictHostKeyChecking=accept-new ${PRIVKEY_OPTION} "${USER}@${TARGET_DEVICE}" "${REMOTE_COMMAND}"
+	sshpass "${SSHPASS_OPTIONS}" ssh ${SSH_OPTIONS} ${PRIVKEY_OPTION} "${USER}@${TARGET_DEVICE}" "${REMOTE_COMMAND[@]}"
 else
-	# shellcheck disable=SC2086
-	ssh -o LogLevel=Error -o StrictHostKeyChecking=accept-new ${PRIVKEY_OPTION} "${USER}@${TARGET_DEVICE}" "${REMOTE_COMMAND}"
+	# shellcheck disable=SC2086,SC2029
+	ssh ${SSH_OPTIONS} ${PRIVKEY_OPTION} "${USER}@${TARGET_DEVICE}" "${REMOTE_COMMAND[@]}"
 fi
